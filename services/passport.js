de09b1d26504a03a1bc9f59f24c0d62fe375fcd3
unknown <jjhernandez94@gmail.com>
2018-07-29 21:54:42 -0700

    refactored google strategy async function and promises

--- services/passport.js
+++ services/passport.js
@@ -23,19 +23,17 @@ passport.use(
       callbackURL: "/auth/google/callback",
       proxy: true
     },
-    (accessToken, refreshToken, profile, done) => {
+    async (accessToken, refreshToken, profile, done) => {
+      const existingUser = await User.findOne({ loginId: profile.id });
 
-      User.findOne({ loginId: profile.id }).then(existingUser => {
-        if (existingUser) {
-          // we already have a record with the given profile ID
-          done(null, existingUser);
-        } else {
-          // we don't have a user record with this ID, make a new record
-          new User({ loginId: profile.id })
-            .save()
-            .then(user => done(null, user));
-        }
-      });
+      if (existingUser) {
+        // we already have a record with the given profile ID
+        done(null, existingUser);
+      } else {
+        // we don't have a user record with this ID, make a new record
+        const user = await new User({ loginId: profile.id }).save();
+        done(null, user);
+      }
     }
   )
 );
