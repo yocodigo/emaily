f2b845391b02f6cbe965d5079d9066697f1221d7
unknown <jjhernandez94@gmail.com>
2018-07-29 21:57:00 -0700

    refactored google strategy conditional statement

--- services/passport.js
+++ services/passport.js
@@ -27,13 +27,11 @@ passport.use(
       const existingUser = await User.findOne({ loginId: profile.id });
 
       if (existingUser) {
-        // we already have a record with the given profile ID
-        done(null, existingUser);
-      } else {
-        // we don't have a user record with this ID, make a new record
-        const user = await new User({ loginId: profile.id }).save();
-        done(null, user);
+        return done(null, existingUser);
       }
+
+      const user = await new User({ loginId: profile.id }).save();
+      done(null, user);
     }
   )
 );
