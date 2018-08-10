2dae4b1572af42aadca4d30ad7468df980dc1c38
unknown <jjhernandez94@gmail.com>
2018-08-10 13:41:27 -0700

    Refactor App from functional to stateful component in order to centralize the use of the action creator

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,6 +1,10 @@
 import axios from 'axios';
 import { FETCH_USER } from './types';
 
-const fetchUser = () => {
-    axios.get('/api/current_user');
+// Redux Thunk inspects value returned from fetchUser action creator
+const fetchUser = () => { // Action creator called
+    return function(dispatch) { // Redux sees that function is being returned and will automatically call it with dispatch
+        axios.get('/api/current_user') // Request is then made
+            .then(res => dispatchEvent({ type: FETCH_USER, payload: res })); // Once we get a response, only then is action dispatched
+    };
 };
--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 
 import Header from './Header';
@@ -6,19 +6,29 @@ const Dashboard = () => <h2>Dashboard</h2>
 const SurveyNew = () => <h2>SurveyNew</h2>
 const Landing = () => <h2>Landing</h2>
 
-const App = () => {
-    return (
-        <div>
-            <BrowserRouter>
-                <div>
-                    <Header />
-                    <Route exact path="/" component={Landing} /> {/* exact is the same as exact={true} */} 
-                    <Route exact path="/surveys" component={Dashboard} />
-                    <Route path="/surveys/new" component={SurveyNew} />
-                </div>
-            </BrowserRouter>
-        </div>
-    );
+
+
+class App extends Component { /* Refactor from functional component to stateful component in order to centralize the use of our action creator. 
+If the header were the only component that cared about whether the user was logged in or not, we would add the action creator there but in this app
+other components also have use for it.*/
+    componentDidMount() {
+
+    }
+    
+    render() {
+        return (
+            <div>
+                <BrowserRouter>
+                    <div>
+                        <Header />
+                        <Route exact path="/" component={Landing} /> {/* exact is the same as exact={true} */} 
+                        <Route exact path="/surveys" component={Dashboard} />
+                        <Route path="/surveys/new" component={SurveyNew} />
+                    </div>
+                </BrowserRouter>
+            </div>
+        );
+    }    
 };
 
 export default App;
