e3fa30b302fe01de4203773ca0b8d590039644b4
unknown <jjhernandez94@gmail.com>
2018-08-12 15:33:19 -0700

    four-step refactoring of src/actions/index.js, using the following arrow function and async/await rules: 1. One expression in arrow function that includes a return statement, 2. If arrow function only has one argument, you can remove parenthesis, 3. Define function with arrow instead of using 'function' keyword, 4. If function is asynchronous, you can add 'async' before function argument, and 'await' keyword before the get request and assign to a variable.

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,11 +1,18 @@
-import axios from "axios";
-import { FETCH_USER } from "./types";
+import axios from 'axios';
+import { FETCH_USER } from './types';
 
 // Redux Thunk inspects value returned from fetchUser action creator
-export const fetchUser = () => { // Action creator called
-  return function(dispatch) { // Redux sees that function is being returned and will automatically call it with dispatch
-    axios
-      .get("/api/current_user") // Request is then made
-      .then(res => dispatch({ type: FETCH_USER, payload: res })); // Once we get a response, only then is action dispatched
-  };
+// export const fetchUser = () => { // Action creator called
+//     return function(dispatch) { // Redux sees that function is being returned and will automatically call it with dispatch
+//         axios.get('/api/current_user') // Request is then made
+//             .then(res => dispatchEvent({ type: FETCH_USER, payload: res })); // Once we get a response, only then is action dispatched
+//     };
+// };
+
+//REFACTORED from ^^^
+export const fetchUser = () => async dispatch => {
+    const res = await axios.get("/api/current_user"); // Request is then made
+    
+    dispatch({ type: FETCH_USER, payload: res }); // Once we get a response, only then is action dispatched
 };
+    
