ffbdeca58a84c2db0f5f52ef3bd981fa39956637
unknown <jjhernandez94@gmail.com>
2018-08-12 21:03:00 -0700

    Hooking up connect from react-redux in Header.js so it can access state. Also did some ES6 refactoring on function mapStateToProps--passed in an object and since return value had an object key with the same label as its value, that got condenced using only single instance of the label.

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,7 +1,20 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class Header extends Component {
+    renderContent() {
+        switch (this.props.auth) {
+            case null:
+                return 'Still deciding';
+            case false:
+                return 'Im loggedout';
+            default: 
+                return 'Im logged in';
+        }
+    }
+
     render() {
+        console.log(this.props);
         return (
             <nav>
                 <div className="nav-wrapper">
@@ -9,9 +22,7 @@ class Header extends Component {
                         Emaily
                     </a>
                     <ul className="right">
-                        <li>
-                            <a>Login With Google</a>
-                        </li>
+                        {this.renderContent()}
                     </ul>
                 </div>    
             </nav>
@@ -19,4 +30,8 @@ class Header extends Component {
     }
 }
 
-export default Header;
+function mapStateToProps({ auth }) {
+    return { auth };
+}
+
+export default connect(mapStateToProps)(Header);
