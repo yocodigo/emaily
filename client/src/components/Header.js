1267d17f7fd88d9ca32cc58d4703b3782cee37db
unknown <jjhernandez94@gmail.com>
2018-08-12 21:12:40 -0700

    Added header content

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,37 +1,37 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React, { Component } from "react";
+import { connect } from "react-redux";
 
 class Header extends Component {
-    renderContent() {
-        switch (this.props.auth) {
-            case null:
-                return 'Still deciding';
-            case false:
-                return 'Im loggedout';
-            default: 
-                return 'Im logged in';
-        }
-    }
-
-    render() {
-        console.log(this.props);
+  renderContent() {
+    switch (this.props.auth) {
+      case null:
+        return;
+      case false:
         return (
-            <nav>
-                <div className="nav-wrapper">
-                    <a className="left brand-logo">
-                        Emaily
-                    </a>
-                    <ul className="right">
-                        {this.renderContent()}
-                    </ul>
-                </div>    
-            </nav>
+          <li>
+            <a href="/auth/google">Login With Google</a>
+          </li>
         );
+      default:
+        return <li><a>Logout</a></li>;
     }
+  }
+
+  render() {
+    console.log(this.props);
+    return (
+      <nav>
+        <div className="nav-wrapper">
+          <a className="left brand-logo">Emaily</a>
+          <ul className="right">{this.renderContent()}</ul>
+        </div>
+      </nav>
+    );
+  }
 }
 
 function mapStateToProps({ auth }) {
-    return { auth };
+  return { auth };
 }
 
 export default connect(mapStateToProps)(Header);
