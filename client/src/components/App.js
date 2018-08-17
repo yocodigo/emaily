7b83175612f53fb57b6135f87977e1ff3331e3b5
unknown <jjhernandez94@gmail.com>
2018-08-17 13:58:44 -0700

    style edit to header

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -7,7 +7,6 @@ import Header from './Header';
 const Dashboard = () => <h2>Dashboard</h2>
 const SurveyNew = () => <h2>SurveyNew</h2>
 
-
 class App extends Component { /* Refactor from functional component to stateful component in order to centralize the use of our action creator. 
 If the header were the only component that cared about whether the user was logged in or not, we would add the action creator there but in this app
 other components also have use for it.*/
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -3,6 +3,10 @@ import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 import Payments from "./Payments";
 
+const styles = {
+  color: "blue"
+}
+
 class Header extends Component {
   renderContent() {
     switch (this.props.auth) {
@@ -16,7 +20,7 @@ class Header extends Component {
         );
       default:
         return [
-          <li> <Payments /> </li>,
+          <li> <Payments /> </li>, 
           <li>
             <a href="/api/logout">Logout</a>
           </li>
@@ -28,7 +32,7 @@ class Header extends Component {
     console.log(this.props);
     return (
       <nav>
-        <div className="nav-wrapper">
+        <div className="nav-wrapper" style={{backgroundColor:"rgb(111, 160, 235)"}}>
           <Link
             to={this.props.auth ? "/surveys" : "/"}
             className="left brand-logo"
