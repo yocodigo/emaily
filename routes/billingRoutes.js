70950036556e61654c983ef5b1ba38bebd6aab82
unknown <jjhernandez94@gmail.com>
2018-11-13 16:33:49 -0800

    requireLogin as an argument to the request handler

--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -1,12 +1,10 @@
 const keys = require("../config/keys");
 const stripe = require("stripe")(keys.stripeSecretKey);
+const requireLogin = require('../middlewares/requireLogin');
 
 module.exports = app => {
-    app.post("/api/stripe", async (req, res) => {
-        if(!req.user){
-            return res.status(401).send({ error: 'You must log in!'});
-        }
-        
+    app.post("/api/stripe", requireLogin, async (req, res) => {
+        // ensure that the user is logged in before adding credits
         const charge = await stripe.charges.create({
             amount: 500,
             currency: 'usd',
