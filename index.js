846365dba2f149ca922151febbda620944d8430f
unknown <jjhernandez94@gmail.com>
2019-01-07 14:20:15 -0800

    Added a catch a route in case request isn't found in authRoutes, billingRoutes or client/build

--- index.js
+++ index.js
@@ -30,6 +30,20 @@ app.use(passport.session());
 require("./routes/authRoutes")(app);
 require("./routes/billingRoutes")(app);
 
+if (process.env.NODE_ENV === 'production') {
+  // Express will serve up production assets
+  // like our main.js file, or main.css file!
+  app.use(express.static('client/build'));
+
+  // Express will serve up the index.html file
+  // if it doesn't recognize the route
+  const path = require('path'); // catch all case if request not found in authRoutes, billingRoutes, or some specific file 
+  //in 'client/build' 
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  })
+}
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, function() {
   console.log("express is working on port " + PORT);
