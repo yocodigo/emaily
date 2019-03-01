59b5d8706cee1cea42cf620b068e9f2d2aef4718
unknown <jjhernandez94@gmail.com>
2019-02-28 16:11:41 -0800

    ADDITION
    Built the SurveyFormReview component, added data persistence when user wants to go back and change the information they entered on the form using destroyOnUnmount: false in the SurveyNew component, added submit form data functionality

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -5,17 +5,11 @@ import { reduxForm, Field } from "redux-form";
 import { Link } from "react-router-dom";
 import SurveyField from "./SurveyField";
 import validateEmails from '../../utils/validateEmails';
-
-const FIELDS = [
-  { label: "Survey Title", name: "title" },
-  { label: "Subject Line", name: "subject" },
-  { label: "Email Body", name: "body" },
-  { label: "Recipient List", name: "emails" }
-];
+import formFields from './formFields';
 
 class SurveyForm extends Component {
   renderFields() {
-    return _.map(FIELDS, ({ label, name }) => {
+    return _.map(formFields, ({ label, name }) => {
       return (
         <Field
           key={name}
@@ -31,7 +25,7 @@ class SurveyForm extends Component {
   render() {
     return (
       <div>
-        <form onSubmit={this.props.handleSubmit(values => console.log(values))}>
+        <form onSubmit={this.props.handleSubmit(this.props.onSurveySubmit)}>
           {this.renderFields()}
           <Link to="/surveys" className="red btn-flat white-text">
           Cancel
@@ -47,20 +41,19 @@ class SurveyForm extends Component {
 function validate(values) {
   const errors = {};
 
-  errors.emails = validateEmails(values.emails || ' ');
+  errors.recipients = validateEmails(values.recipients || ' ');  
   
-  _.each(FIELDS, ({ name  }) => { // instead of writing out multiple if statements, loop thru FIELDS
+  _.each(formFields, ({ name }) => { // instead of writing out multiple if statements, loop thru formFields
     if (!values[name]){
       errors[name] = 'You must provide a value'; // generic error approach
     }
   });
-
-  
 
   return errors; // if empty object, no errors
 }
 
 export default reduxForm({
   validate,
-  form: "surveyForm"
+  form: "surveyForm",
+  destroyOnUnmount: false
 })(SurveyForm);
